938c66252bd6a58c1b2bdc5ed91bbef73e151a04
Omkar Parab <omkar211196@gmail.com>
2020-12-30 11:15:01 +0530

    Update vueapp.js

--- API fetch/vueapp.js
+++ API fetch/vueapp.js
@@ -9,7 +9,7 @@ new Vue({
 
         async getAllStarWarsFilms(){
 
-            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token='IGQVJWNHFnaEJ1VEQzMW91cWpzRlZAtMGcxeHo0VHR3S3RRaXFiTHJHcVAzVGRkNjVfSGN2RHFSZAmtmdDY5T0xHeVVCQzJXaGtGMjQ3TTJaNi0yM1pLal8zZAS1HSjZAYckRuRDAtSWRnT3dkTHlQczdYdAZDZD')
+            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token=IGQVJWNHFnaEJ1VEQzMW91cWpzRlZAtMGcxeHo0VHR3S3RRaXFiTHJHcVAzVGRkNjVfSGN2RHFSZAmtmdDY5T0xHeVVCQzJXaGtGMjQ3TTJaNi0yM1pLal8zZAS1HSjZAYckRuRDAtSWRnT3dkTHlQczdYdAZDZD')
             .then(response=>response.json())
 
             .then(data=>{
