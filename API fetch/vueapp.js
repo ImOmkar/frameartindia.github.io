b320b12e653843fc1d5bd400a9d67fb6134cc148
Omkar Parab <42148962+ImOmkar@users.noreply.github.com>
2021-05-12 12:06:36 +0530

    Update vueapp.js

--- API fetch/vueapp.js
+++ API fetch/vueapp.js
@@ -9,7 +9,7 @@ new Vue({
 
         async getAllStarWarsFilms(){
 
-            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token=${{ env.DISPLAY_ACCESS_TOKEN }}')
+            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token=IGQVJVanNGUkZASaUhaU0tHeFBMaEZASTE04Nkh6end6dVRHS1VZAa2pQRkJIZAm8yUWNOQndBLWdORWY5RFVkRTN1TFBEcC1sTllwbm9vaW40V3J1cEFhMzJueXNmYzhGU3dibkNlNmlSVlMxenkyUEZAkdgZDZD')
             .then(response=>response.json())
 
             .then(data=>{
