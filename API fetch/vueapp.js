0a0400b14e209625bc1f5ca3fb10cfe9b8ce00dc
Omkar Parab <42148962+ImOmkar@users.noreply.github.com>
2021-05-12 11:41:54 +0530

    moved environment variables in github secret

--- API fetch/vueapp.js
+++ API fetch/vueapp.js
@@ -9,7 +9,7 @@ new Vue({
 
         async getAllStarWarsFilms(){
 
-            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token=IGQVJVaTdXdFRQcXBPR0lSUHFBNEN1alF0NmYtZAlJKZAjZAoMDVpdTB6a0F3Q1FvaTFKa1pVN3dCV1c3ZAEctclRtYXlMMGkzVGpPbjlEd2w0VGlkLWZAwbDBhZAVFxQ0ZAxUElBV19VRUo2TDlEaFhmMjZAVUgZDZD')
+            fetch('https://graph.instagram.com/me/media?fields=media_url&access_token=${{ secrets.DISPLAY_ACCESS_TOKEN }}')
             .then(response=>response.json())
 
             .then(data=>{
